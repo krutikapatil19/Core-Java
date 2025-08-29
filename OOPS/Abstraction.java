0809f191376d0be9df499fb11915a7e0b505b012
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-08-29 22:23:41 +0530

    Abstraction in Java

--- OOPS/Abstraction.java
+++ OOPS/Abstraction.java
@@ -1,18 +1,18 @@
-//Abstraction = hiding implementation
+//Abstraction = hiding implementation (hiding the details and only showing the essentials)
 
 
-//Abstract class
+//Abstract class (needs subclasses to complete)
 abstract class Vehicle {
-    abstract void drive ();             //no body (only declaration)
+    abstract void drive ();                                 //no body (only declaration)
 }
 class Car extends Vehicle {
     void drive() {
-        System.out.println("Car is driving");
+        System.out.println("Car is driving");            //the vehicle is just a blueprint , and the car is real object .
     }
 }
 
 
-//Interface
+//Interface = a contract/rulebook . (every class must follow)
 
 interface Payment {
     void pay();
@@ -28,11 +28,11 @@ public class Abstraction {
     public static void main(String[] args) {
 
         //Using abstract class and its implementation
-        Vehicle v = new Car();  
-        v.drive();
+        Vehicle v = new Car();                              //Vehicle reference , Car object
+        v.drive();                                          //"Car is driving"
 
         //Using interface and its implementation
-        Payment p = new UPI();
-        p.pay();
+        Payment p = new UPI();                              //Payment reference, UPI object 
+        p.pay();                                            //"Payment done via UPI"
     }
 }

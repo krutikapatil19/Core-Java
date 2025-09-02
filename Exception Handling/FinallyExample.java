fd16c11b75a16000989119589a6a7b5abe98e214
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-09-02 11:41:11 +0530

    Finally block example

--- Exception Handling/FinallyExample.java
+++ Exception Handling/FinallyExample.java
@@ -1,3 +1,4 @@
+//In java, finally block is always executed no matter whether there is an exception or not.
 public class FinallyExample {
     public static void main(String[] args) {
         try {
@@ -5,7 +6,13 @@ public class FinallyExample {
         } catch (ArithmeticException e) {
             System.out.println("Error handled");
         } finally {
-            System.out.println("This will always run");
+            System.out.println("This is the finally block .This will always run");
         }
     }
 }
+
+
+/*If an exception occurs, the finally block is executed after the try..catch block. 
+Otherwise, it is executed after the try block .
+For each try block, there can be only one finally block.
+*/

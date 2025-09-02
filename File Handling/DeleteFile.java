5350b14f5f472e15ac971c90260a5a3deedfd2e0
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-09-02 20:58:03 +0530

    Delete file

--- File Handling/DeleteFile.java
+++ File Handling/DeleteFile.java
@@ -4,8 +4,8 @@ public class DeleteFile {
     public static void main(String[] args) {
         File f = new File("myfile.txt");
 
-        if(f.delete()) {
-            System.out.println("Deleted the file: " + f.getName());
+        if(f.delete()) {                                                    //f.delete -> deletes the file.
+            System.out.println("Deleted the file: " + f.getName());         //returns true if success, false if failed.
         } else {
             System.out.println("Failed to delete the file.");
         }

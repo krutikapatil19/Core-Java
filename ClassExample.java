f61f29f66029417dc61050568c3c0bab1bdbebb1
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-09-04 22:21:55 +0530

    Class Example in OOP

--- ClassExample.java
+++ ClassExample.java
@@ -1,20 +1,22 @@
-class Car {
-    //Properties/variables of the class
+class Car {                                         //creating a class named Car (Car = blueprint/design)
+    
+    //Varibales inside class (called roperties/attributes)
     String brand;
     int year;
 
-    //Method (action/behavior of the class)
+    //Method inside class (action/behavior of the car class)
     void displayInfo() {
         System.out.println("Brand: " + brand + " , Year : " + year);
     }
 }
 
+//Main class
 public class ClassExample {
     public static void main(String[] args) {
-        //Creating an object of class Car
+        //Creating an object of class Car(real things from blueprint)
         Car myCar = new Car();
 
-        //Assigning values to the object
+        //Assigning values to the object(initialize its properties)
         myCar.brand = "Tesla" ;
         myCar.year = 2024;
 

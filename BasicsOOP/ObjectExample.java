54d339333a4be935163b0df93b688dd8e0cda7c2
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-09-06 18:41:26 +0530

    Object Example in OOP

--- BasicsOOP/ObjectExample.java
+++ BasicsOOP/ObjectExample.java
@@ -1,6 +1,26 @@
-package BasicsOOP;
+class Student {
+    String name;
+    int age;
 
-public class ObjectExample {
-    
+    void showDetails() {
+        System.out.println("Name: " + name + " , Age : " + age);
+    }
 }
 
+public class ObjectExample {
+    public static void main(String[] args) {
+        //Creating first object
+        Student s1 = new Student();
+        s1.name = "Eliza";
+        s1.age = 20;
+
+        //Creating second object
+        Student s2 = new Student();
+        s2.name = "John";
+        s2.age = 21;
+
+        //Calling methods
+        s1.showDetails();
+        s2.showDetails(); 
+    }
+}

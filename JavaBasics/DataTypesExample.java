f57c53bd7bbe97032ab7122709de9368dcb45189
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-09-08 22:28:36 +0530

    Data Types Exampe

---
+++ JavaBasics/DataTypesExample.java
@@ -0,0 +1,24 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+
+        //int = whole numbers 
+        int apples = 5;
+        System.out.println("Apples: " + apples);
+
+        //float = decimal numbers
+        float milk = 2.5f;
+        System.out.println("Milk litres: " + milk);
+
+        //char = single character
+        char grade = 'A';
+        System.out.println("Grade: " + grade);
+
+        //boolean = true or false
+        boolean isFresh = true;
+        System.out.println("Is the fruits fresh ? " + isFresh);
+
+        //String = text
+        String fruitName = "Mango";
+        System.out.println("Fruit: " + fruitName);
+    }
+}

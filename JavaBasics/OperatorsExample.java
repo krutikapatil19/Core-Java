6d25d846d8b948226bbbc9bcfcf63e8f76f35eda
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-09-08 22:53:23 +0530

    Operators in Java

--- JavaBasics/OperatorsExample.java
+++ JavaBasics/OperatorsExample.java
@@ -4,20 +4,20 @@
 
 public class OperatorsExample {
     public static void main(String[] args) {
-        int a =10, b = 3;
+        int a = 10, b = 3;
 
-        //Arithmetic Operators
-        System.out.println("Addition: " + (a+b));
-        System.out.println("Subtraction: " + (a-b));
-        System.out.println("Multiplication: " + (a*b));
-        System.out.println("Division: " + (a/b));
-        System.out.println("Modulo: " + (a%b));
+        // Arithmetic Operators
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulo: " + (a % b));
 
-        //Comparison Operators
-        System.out.println("a > b?"  + (a>b));
-        System.out.println("a==b" + (a==b));
+        // Comparison Operators
+        System.out.println("a > b?" + (a > b));
+        System.out.println("a==b" + (a == b));
 
-        //Logical operators
+        // Logical operators
         boolean x = true, y = false;
         System.out.println("x AND y:" + (x && y));
         System.out.println("x OR y: " + (x || y));

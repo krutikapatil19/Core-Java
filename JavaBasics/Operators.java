15d31b31b34f32c4a385a715a61abe3d6d4adf25
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-09-08 22:40:06 +0530

    Operators in Java

---
+++ JavaBasics/Operators.java
@@ -0,0 +1,4 @@
+//Operators are used to perform computations or compare values.
+//Arithmetic operators are used for mathematical calculations.
+//Logical operators are used in decision -making or boolean expressions.
+
